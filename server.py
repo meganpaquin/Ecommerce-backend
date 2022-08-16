b2436212e0a43b7e83b8dab570d7b3f03d65cc30
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-08-15 22:56:23 -0400

    Add catalog and iterate a list

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 from data import me
+from data import catalog
 
 app = Flask(__name__)
 
@@ -29,7 +30,50 @@ def test_api():
 def about_api():
     return json.dumps(me)
 
+@app.get("/api/catalog")
+def get_catalog():
+    # return the list of products
+    return json.dumps(catalog)
 
-app.run(debug=True)
-# This is starting the server... this should be the last thing on the file
-# remove debug for production
+@app.get('/api/product/<id>')
+def get_product_byid(id):
+    # the two id names must match above
+    for prod in catalog:
+        if prod["_id"] == str(id):
+            return json.dumps(prod)
+    return 'No item exists'
+
+@app.get('/api/products/<category>')
+def get_by_category(category):
+    results = []
+    for prod in catalog:
+        if prod["category"].lower() == category.lower():
+            results.append(prod)
+
+    return json.dumps(results)
+
+
+@app.get("/api/count") 
+def catalog_count():
+    # return the number of items in a list
+    number = len(catalog)
+    return json.dumps(number)
+
+@app.get("/api/catalog/total")
+def total_inventory_price():
+    total = 0
+    for price in catalog:
+        total += price["price"]
+
+    return json.dumps(total)
+
+
+@app.get("/api/catalog/cheap")
+def cheapest():
+    holder = catalog[0]
+    for price in catalog:
+        if price["price"] < holder["price"]:
+            holder = price
+    
+    return json.dumps(holder)
+            
